5d8d9a3778f9b55b0b4e4a8ccd0f3b6f3fe96622
hgoc <ofirc36@gmail.com>
2020-11-23 23:24:48 +0200

    Fixed bug with clear and undo buttons and started wokring on making the calculator actually work

--- script.js
+++ script.js
@@ -1,13 +1,49 @@
 const buttons = document.querySelectorAll(".calc-button");
 const display = document.querySelector("#result-paragraph");
+const clearDisplayButton = document.querySelector("#clear-button");
+const undoButton = document.querySelector("#undo-button");
+const numberButtons = document.querySelectorAll(".calc-num");
+const operButtons = document.querySelectorAll(".calc-oper");
+let evaluation = [];
+let tempOperand = "";
 
-console.log("Script loaded");
-
+// When a button is clicked, add it to the display
 buttons.forEach(button => {
     button.addEventListener("click", function(clickTarget) {
-        display.textContent += clickTarget.target.textContent;
+        if (clickTarget.target.id != "clear-button" && clickTarget.target.id != "undo-button") {
+            display.textContent += clickTarget.target.textContent;
+        }
+        if (Array.from(clickTarget.target.classList).includes("calc-num")) {
+            tempOperand = tempOperand.concat(clickTarget.target.textContent);
+        } else if (Array.from(clickTarget.target.classList).includes("calc-oper") && tempOperand != "") {
+            evaluation.push(tempOperand);
+            tempOperand = "";
+            evaluation.push(clickTarget.target.textContent);
+            console.log(evaluation);
+        }
     });
 })
+
+// Clear the display when "C" buttons is clicked
+clearDisplayButton.addEventListener("click", function(clickTarget) {
+    display.textContent = "";
+});
+
+/// Remove the last character from the display string
+undoButton.addEventListener("click", function(clickTarget) {
+    display.textContent = display.textContent.slice(0, -1);
+});
+
+// numberButtons.addEventListener("click", function(clickTarget) {
+//     tempButton = tempButton + clickTarget.textContent;
+// });
+
+// operButtons.addEventListener("click", function(clickTarget) {
+//     evaluation.push(tempOperand);
+//     tempOperand = "";
+//     evaluation.push(clickTarget.textContent);
+//     console.log(evaluation);
+// });
 
 function add(number1, number2) {
     return number1 + number2;

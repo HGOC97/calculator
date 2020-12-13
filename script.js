5d34df8c9adbef1dc9e5b5c3a3a6549761df37a9
hgoc <ofirc36@gmail.com>
2020-12-13 18:46:49 +0200

    Added comments and display

--- script.js
+++ script.js
@@ -1,66 +1,99 @@
-const buttons = document.querySelectorAll(".calc-button");
-const display = document.querySelector("#result-paragraph");
-const clearDisplayButton = document.querySelector("#clear-button");
-const undoButton = document.querySelector("#undo-button");
 const numberButtons = document.querySelectorAll(".calc-num");
 const operButtons = document.querySelectorAll(".calc-oper");
-let evaluation = [];
-let tempOperand = "";
+const clearButton = document.querySelector("#clear-button");
+const resultDisplay = document.querySelector("#result-paragraph");
+const equalButton = document.querySelector("#equals-button");
 
-// When a button is clicked, add it to the display
-buttons.forEach(button => {
+let operator = "";
+let tempOperand = "";
+let firstOperand = "";
+let secondOperand = "";
+
+numberButtons.forEach(button => {
     button.addEventListener("click", function(clickTarget) {
-        if (clickTarget.target.id != "clear-button" && clickTarget.target.id != "undo-button") {
-            display.textContent += clickTarget.target.textContent;
-        }
-        if (Array.from(clickTarget.target.classList).includes("calc-num")) {
-            tempOperand = tempOperand.concat(clickTarget.target.textContent);
-        } else if (Array.from(clickTarget.target.classList).includes("calc-oper") && tempOperand != "") {
-            evaluation.push(tempOperand);
-            tempOperand = "";
-            evaluation.push(clickTarget.target.textContent);
-            console.log(evaluation);
+        // If the operation wasn't selected
+        let arrayOperand = Array.from(tempOperand);
+        // If no floating point in operand
+        if (!arrayOperand.includes(".")) {
+            tempOperand += clickTarget.target.textContent;
+            resultDisplay.textContent = tempOperand;
+            if (firstOperand != "" && operator != "" && secondOperand === "") {
+                resultDisplay.textContent = `${firstOperand}${operator}${tempOperand}`
+            }
+            // If there is a floating point
+        } else {
+            if (clickTarget.target.textContent != ".") {
+                tempOperand += clickTarget.target.textContent;
+                resultDisplay.textContent = tempOperand;
+            }
         }
     });
-})
-
-// Clear the display when "C" buttons is clicked
-clearDisplayButton.addEventListener("click", function(clickTarget) {
-    display.textContent = "";
 });
 
-/// Remove the last character from the display string
-undoButton.addEventListener("click", function(clickTarget) {
-    display.textContent = display.textContent.slice(0, -1);
+// When an operation button is pressed, assign it as an operation for the coming calc
+operButtons.forEach(button => {
+    button.addEventListener("click", function(clickTarget) {
+        // If tempOperand has a value (meaning calc isn't empty)
+        if (tempOperand != "") {
+            // If there is no firstOperand
+            if (firstOperand == "") {
+                firstOperand = tempOperand;
+            // If there is no secondOperand
+            } else {
+                secondOperand = tempOperand;
+            }
+            tempOperand = "";
+            // If there are values for both firstOperand and secondOperand then calculate
+            if (firstOperand != "" && secondOperand != "") {
+                let result = operate(operator, firstOperand, secondOperand);
+                firstOperand = result;
+                secondOperand = "";
+            }
+            // Set the next operator
+            operator = clickTarget.target.textContent;
+            resultDisplay.textContent += operator;
+            console.log(`firstOperand is ${firstOperand} secondOperand is ${secondOperand}`);
+        }
+    });
 });
 
-// numberButtons.addEventListener("click", function(clickTarget) {
-//     tempButton = tempButton + clickTarget.textContent;
-// });
+// Reset all values when clear button is clicked
+clearButton.addEventListener("click", function(clickTarget) {
+    operator = "";
+    tempOperand = "";
+    firstOperand = "";
+    secondOperand = "";
+    resultDisplay.textContent = "";
+});
 
-// operButtons.addEventListener("click", function(clickTarget) {
-//     evaluation.push(tempOperand);
-//     tempOperand = "";
-//     evaluation.push(clickTarget.textContent);
-//     console.log(evaluation);
-// });
+equalButton.addEventListener("click", function(clickTarget) {
+    if (firstOperand != "" && tempOperand != "" && operator != "") {
+        let result = operate(operator, firstOperand, tempOperand);
+        firstOperand = result;
+        resultDisplay.textContent = firstOperand;
+        secondOperand = "";
+    }
+});
+
+// Operations //
 
 function add(number1, number2) {
-    return number1 + number2;
+    return parseFloat(number1) + parseFloat(number2);
 }
 
 function subtract(number1, number2) {
-    return number1 - number2;
+    return parseFloat(number1) - parseFloat(number2);
 }
 
 function multiply(number1, number2) {
-    return number1 * number2;
+    return parseFloat(number1) * parseFloat(number2);
 }
 
 function divide(number1, number2) {
-    return number1 / number2;
+    return parseFloat(number1) / parseFloat(number2);
 }
 
+// Perform the appropriate operation
 function operate(operator, number1, number2) {
     let result;
     switch (operator) {
@@ -70,7 +103,7 @@ function operate(operator, number1, number2) {
         case "-":
             result = subtract(number1, number2);
             break;
-        case "*":
+        case "X":
             result = multiply(number1, number2);
             break;
         case "/":
